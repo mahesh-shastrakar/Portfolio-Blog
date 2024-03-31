083a4136da00ebb37c9b8ca21b1feadf313138ac
mahesh <maheshshastrakar123@gmail.com>
2024-03-31 20:22:20 +0530

    fixed

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ mongoose
     console.log(err);
   });
 
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 app.listen(process.env.PORT, () => {
   console.log(`Server running on http://localhost:${process.env.PORT}`);
 });
