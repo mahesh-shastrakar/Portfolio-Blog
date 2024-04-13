9925773c521f4b7fc554da976eb521a78934a87b
mahesh <maheshshastrakar123@gmail.com>
2024-04-13 15:20:51 +0530

    modified the proxy

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://portfolio-blog-backend-three.vercel.app/",
+        target: "https://portfolio-blog-backend-three.vercel.app",
         secure: false,
       },
     },
