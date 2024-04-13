afdcd1271690809a4fd4deca68e16fd15053e98b
mahesh <maheshshastrakar123@gmail.com>
2024-04-13 15:43:20 +0530

    added vite server change origin

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "https://portfolio-blog-backend-three.vercel.app",
+        changeOrigin: true,
         secure: false,
       },
     },
