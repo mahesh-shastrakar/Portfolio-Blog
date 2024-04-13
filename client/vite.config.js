1c5a37d93e44a172c689a4e032b122a3307997bd
mahesh <maheshshastrakar123@gmail.com>
2024-04-13 15:40:30 +0530

    sss

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://portfolio-blog-backend-three.vercel.app",
+        target: "https://portfolio-blog-backend-three.vercel.app",
         secure: false,
       },
     },
