95ad63a1cc669568ef886777c67cd68a525787c6
mahesh <maheshshastrakar123@gmail.com>
2024-07-30 02:11:02 +0530

    updated sign in page

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -10,35 +10,32 @@ import { useDispatch, useSelector } from "react-redux";
 import OAuth from "../components/OAuth";
 const SignIn = () => {
   const [formData, setFormData] = useState({});
-  const navigate = useNavigate();
+  const { loading, error: errorMessage } = useSelector((state) => state.user);
   const dispatch = useDispatch();
-  const { loading, error: errorMessage } = useSelector(
-    (state) => state.currentUser
-  );
+  const navigate = useNavigate();
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
   const handleSubmit = async (e) => {
     e.preventDefault();
     if (!formData.email || !formData.password) {
-      return dispatch(signInFailure("Please fill in all the fields"));
+      return dispatch(signInFailure("Please fill all the fields"));
     }
     try {
       dispatch(signInStart());
-      const response = await fetch("/api/auth/login", {
+      const res = await fetch("/api/auth/signin", {
         method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
+        headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
       });
-      const data = await response.json();
-      if (response.ok) {
-        dispatch(signInSuccess(data));
-        navigate("/");
-      }
+      const data = await res.json();
       if (data.success === false) {
         dispatch(signInFailure(data.message));
+      }
+
+      if (res.ok) {
+        dispatch(signInSuccess(data));
+        navigate("/");
       }
     } catch (error) {
       dispatch(signInFailure(error.message));
@@ -58,53 +55,49 @@ const SignIn = () => {
         </div>
         {/*Right*/}
         <div className="flex-1">
-          <div>
-            <form className=" flex flex-col gap-5 ">
-              <div>
-                <Label value="Your Email"></Label>
-                <TextInput
-                  type="email"
-                  placeholder="abc@xyz.com"
-                  id="email"
-                  onChange={handleChange}
-                />
-              </div>
-              <div>
-                <Label value="Your Password"></Label>
-                <TextInput
-                  type="password"
-                  placeholder="Password"
-                  id="password"
-                  onChange={handleChange}
-                />
-              </div>
-              <Button
-                gradientDuoTone="purpleToPink"
-                className="w-1/2 mx-auto"
-                type="submit"
-                outline
-                onClick={handleSubmit}
-              >
-                {loading ? (
-                  <>
-                    <Spinner className="sm" />
-                    <span className="pl-3">Loading...</span>
-                  </>
-                ) : (
-                  "Sign In"
-                )}
-              </Button>
-              <OAuth />
-            </form>
-            <div className=" text-center gap-2 mt-5">
-              Don't have an account?{" "}
-              <Link to="/signup" className="text-purple-500 underline">
-                Sign Up
-              </Link>
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+            <div>
+              <Label value="Your email" />
+              <TextInput
+                type="email"
+                placeholder="name@company.com"
+                id="email"
+                onChange={handleChange}
+              />
             </div>
+            <div>
+              <Label value="Your password" />
+              <TextInput
+                type="password"
+                placeholder="**********"
+                id="password"
+                onChange={handleChange}
+              />
+            </div>
+            <Button
+              gradientDuoTone="purpleToPink"
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading...</span>
+                </>
+              ) : (
+                "Sign In"
+              )}
+            </Button>
+            <OAuth />
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Dont Have an account?</span>
+            <Link to="/sign-up" className="text-blue-500">
+              Sign Up
+            </Link>
           </div>
           {errorMessage && (
-            <Alert className="mt-5" color={"failure"}>
+            <Alert className="mt-5" color="failure">
               {errorMessage}
             </Alert>
           )}
