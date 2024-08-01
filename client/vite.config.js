bdf5abf94e7afb77086c51c2739fb2eafda879d6
mahesh <maheshshastrakar123@gmail.com>
2024-08-01 23:32:19 +0530

    edited gitignore files and vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dotenv from "dotenv";
+import "dotenv/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // the server proxy is used to redirect the requests to the backend server when the client makes a request to the /api endpoint
   // server: {
   //   proxy: {
-  //     "/api": "http://portfolio-blog-backend-three.vercel.app",
+  // "/api": "process.env.BACKEND_URL",
   //     // "/api": "http://localhost:3000",
   //   },
   // },
